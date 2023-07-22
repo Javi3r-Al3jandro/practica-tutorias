a748e94ff8c13c71748c1ac0f03b68bb9dcc24bf
Ale <ale_6524@hotmail.com>
2023-07-22 08:33:57 -0300

    FORMULARIO CONTROLADO(SE TERMINO EL EJERCICIO PROPUESTO DE CLASE 26)

---
+++ Clase 11 react/React/src/App.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import Navbar from "./Navbar"
+
+
+const App = () => {
+  return (
+    <>    
+    <Navbar></Navbar>
+    <center><u><h1>Clase react</h1></u></center>
+    </>
+  ) 
+}
+
+export default App
